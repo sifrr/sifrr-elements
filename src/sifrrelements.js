ff184a3b7aa73660cace2151d1f1180bdc44e925
Aaditya Taparia <aadityataparia@gmail.com>
2019-04-15 14:44:21 +0900

    chore(package): update file-url to version 3.0.0
    * chore(package): update lockfile yarn.lock

--- src/sifrrelements.js
+++ src/sifrrelements.js
@@ -1,3 +1,4 @@
+// elements
 export { default as SifrrLazzyImg } from '../elements/sifrr-lazy-img/src/sifrrlazyimg';
 export { default as SifrrLazyPicture } from '../elements/sifrr-lazy-picture/src/sifrrlazypicture';
 export { default as SifrrStater } from '../elements/sifrr-stater/src/sifrrstater';
@@ -5,3 +6,5 @@ export { default as SifrrTabs } from '../elements/sifrr-tabs/src/sifrrtabs';
 export { default as SifrrShowcase } from '../elements/sifrr-showcase/src/sifrrshowcase';
 export { default as SifrrCodeEditor } from '../elements/sifrr-code-editor/src/sifrrcodeeditor';
 export { default as SifrrProgressRound } from '../elements/sifrr-progress-round/src/sifrrprogressround';
+
+// helpers
