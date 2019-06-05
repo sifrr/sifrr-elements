5b7d27c9adc3fe1ca1fff5f623c2bc0511d0f287
Aaditya Taparia <aadityataparia@gmail.com>
2019-06-05 16:16:27 +0900

    test: add tests for sifrr include

--- elements/sifrr-include/test/browser/loading.test.js
+++ elements/sifrr-include/test/browser/loading.test.js
@@ -7,4 +7,21 @@ describe('include', function() {
     const name = await page.evaluate("Sifrr.Dom.elements['sifrr-include'].elementName");
     expect(name).to.equal('sifrr-include');
   });
+
+  it('loads html', async function() {
+    const html = await page.$eval('#html', el => el.innerHTML);
+    expect(html).to.equal('<p>OK</p>');
+  });
+
+  it('loads css', async function() {
+    const css = await page.$eval('#css style', el => el.innerHTML);
+    expect(css).to.equal(`a {
+  color: green;
+}`);
+  });
+
+  it('loads js', async function() {
+    const js = await page.$eval('#js script', el => el.innerHTML);
+    expect(js).to.equal('window.ok = \'ok\';');
+  });
 });
