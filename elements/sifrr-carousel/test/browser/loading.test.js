462b69aaab93b71d863988c0e8e6b4cf92b4cd60
Aaditya Taparia <aadityataparia@gmail.com>
2020-01-20 01:27:48 +0900

    chore: carousel

--- elements/sifrr-carousel/test/browser/loading.test.js
+++ elements/sifrr-carousel/test/browser/loading.test.js
@@ -1,9 +1,9 @@
-describe('code editor', function() {
+describe('carousel', function() {
   before(async () => {
     await page.goto(`${PATH}/carousel.html`);
   });
 
-  it('loads code editor', async function() {
+  it('loads carousel', async function() {
     const name = await page.evaluate("Sifrr.Dom.elements['sifrr-carousel'].elementName");
     expect(name).to.equal('sifrr-carousel');
   });
